2c29ebb22a5a468a6adf355633ef7912982cb184
Tim Camper <twcamper@thoughtworks.com>
2013-05-30 22:25:50 -0500

    must wait for player child process; must run player in a thread so the progress line won't be blocked

--- src/series-timer.c
+++ src/series-timer.c
@@ -5,14 +5,17 @@
 #include <string.h>
 #include <errno.h>
 #include <sys/types.h>
+#include <signal.h>
+#include <pthread.h>
 
 void indent(int tabs);
 void wait_showing_progress(int col, int tabs, char *minutes_s);
 char *time_s(char *format);
 void exit_error(char *msg);
 int should_show_tenth_minute(int minutes);
-void kill_all_players();
-void play_random_song(void);
+void kill_all_players(void);
+void kill_player(pid_t);
+void *play_random_song(void *);
 void say(char *what);
 
 #define TIME_FORMAT_24_HR "%H:%M"
@@ -55,13 +58,17 @@ void exit_error(char *msg)
   exit(1);
 }
 
-void play_random_song(void)
+static pid_t SERIES_TIMER__player_pid = 0;
+
+/* This is a thread function, so that the progress line may progress */
+void *play_random_song(void *param)
 {
-  pid_t pid = fork();
+  SERIES_TIMER__player_pid = fork();
+  int pid_status;
   errno = 0;
-  if (pid == -1)
+  if (SERIES_TIMER__player_pid == -1)
     exit_error("Can't fork process to start player");
-  if (pid == 0)  {
+  if (SERIES_TIMER__player_pid == 0)  {
     char *player;
     expand_home_dir(PLAYER, &player);
     errno = 0;
@@ -70,16 +77,23 @@ void play_random_song(void)
     }
     free(player);
   }
+  errno = 0;
+  if (waitpid(SERIES_TIMER__player_pid, &pid_status, 0) == -1) {
+    fprintf(stderr, "Waiting for %d(%d): %s\n", SERIES_TIMER__player_pid, pid_status, strerror(errno));
+  }
+  pthread_exit(0);
 }
 
 void say(char *what)
 {
   int pid_status;
+  errno = 0;
   pid_t pid = fork();
 
   if (pid == -1)
     exit_error("Can't fork process to 'say' something.");
   if (pid == 0)  {
+    errno = 0;
     if (execl(SAYER, SAYER, what, NULL) == -1)  {
       exit_error("Can't start sayer");
     }
@@ -99,8 +113,19 @@ void kill_all_players()
 
   if (pid == 0) {
     errno = 0;
-    if (execlp("bash", "bash", KILLER, "'prf|Audirvana|VLC'", NULL) == -1)
+    if (execlp("bash", "bash", KILLER, "'Audirvana|VLC'", NULL) == -1)
       exit_error("Can't killall players");
+  }
+}
+
+void kill_player(pid_t pid)
+{
+  if (pid) {
+    errno = 0;
+    if (kill(pid, SIGINT) == -1) {
+      fprintf(stderr, "pid %d\t", pid);
+      exit_error("Can't kill player");
+    }
   }
 }
 
@@ -124,7 +149,7 @@ void wait_showing_progress(int col, int tabs, char *minutes_s)
 {
   int minutes = atoi(minutes_s);
   indent(tabs);
-  printf("%d) %s minutes\n", col, minutes_s);
+  printf("%d) %s %s\n", col, minutes_s, (minutes == 1 ? "minute" : "minutes"));
   indent(tabs);
   while ( minutes > 0)
   {
@@ -164,23 +189,40 @@ char *time_s(char *format)
 int main(int argc, char *argv[])
 {
   int i, tabs;
+  pthread_t player_thread;
+  void *thread_result;
+
+  kill_all_players();
+
   for (i = 1, tabs = 0; i < argc; i++, tabs++) {
+    /* this function works because the player runs in a thread */
     wait_showing_progress(i, tabs, argv[i]);
 
-    kill_all_players();
+    /* in case we timeout during a long song */
+    /* (should probably handle the thread somehow, but it *seems* to work) */
+    kill_player(SERIES_TIMER__player_pid);
 
+    /* this will block if Audirvana is playing, thus the kill above */
     say(time_s(TIME_FORMAT_AM_PM));
 
-    play_random_song();
+    /* do NOT join this thread within the loop,
+     * we don't want to wait for it to complete
+     */
+    errno = 0;
+    if (pthread_create(&player_thread, NULL, play_random_song, NULL) == -1)
+      exit_error("pthread_create");
   }
 
   NEWLINE;
   indent(tabs);
   puts("Last");
 
+  /* wait for last thread only */
+  errno = 0;
+  if (pthread_join(player_thread, &thread_result) == -1)
+    exit_error("pthread_join");
+
   sleep(2);
-  /* 'say' is blocked by Audirvana hogging a mutual audio channel,*/
-  /* which is how we keep the song alive until it's done.*/
   say("Finished");
 
   say(time_s(TIME_FORMAT_AM_PM));
